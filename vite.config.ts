5e23dc323aae57d5ebe485c579b7116b25a1ceeb
Rendy Arya Kemal <renrror@gmail.com>
2023-06-29 10:43:06 +0700

    Configure vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-let faviconURL = '/favicon.svg'
-
-
+let faviconURL = "/favicon.svg";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,jpg}"],
+      },
       includeAssets: [faviconURL],
       manifest: {
-        theme_color: '#ffffff',
+        name: "SIAK Mobile",
+        short_name: "SIAK Mobile",
+        description: "SIAK-NG for mobile",
+        theme_color: "#ffdd00",
         icons: [
           {
             src: faviconURL,
-            sizes: '512x512',
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
+            sizes: "512x512",
+            type: "image/svg+xml",
+            purpose: "any maskable",
           },
           {
             src: faviconURL,
-            sizes: '512x512',
-            type: 'image/png',
-          }
-        ]
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
-    })
-  ]
-})
+    }),
+  ],
+});
