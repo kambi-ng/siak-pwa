ed9f14e001421e11b2ad44fe7a5dc45af4c02399
Rendy Arya Kemal <renrror@gmail.com>
2023-06-29 08:08:32 +0700

    Center text on under construction

--- src/components/UnderConstruction.tsx
+++ src/components/UnderConstruction.tsx
@@ -2,7 +2,7 @@ import { Barricade } from "@phosphor-icons/react";
 
 export default function UnderConstruction() {
   return (
-    <div className="flex flex-col items-center justify-center gap-2 my-auto h-[100%]">
+    <div className="flex flex-col items-center justify-center gap-2 my-auto h-[100%] text-center">
       <Barricade size={72} weight="bold" />
       <p>Sorry, this feature is under construction!</p>
     </div>
